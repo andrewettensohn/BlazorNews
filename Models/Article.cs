506bc89a4270655e94af447e3b7ee6b3dfee53a4
Andrew Ettensohn <andrewettensohn@gmail.com>
2020-08-16 21:33:51 -0400

    Add project files.

---
+++ Models/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorNews.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string By { get; set; }
+        public int Descendants { get; set; }
+        public List<int> Kids { get; set; }
+        public int Score { get; set; }
+        public int Time { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+    }
+}
