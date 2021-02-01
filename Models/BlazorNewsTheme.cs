09e06c234bf9552866a1fd4c2342140e341f853e
Andrew Ettensohn <andrewettensohn@gmail.com>
2021-02-01 18:11:01 -0500

    Working on theme toggle

---
+++ Models/BlazorNewsTheme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorNews.Models
+{
+    public class BlazorNewsTheme
+    {
+        public string TextColor { get; set; }
+        public string MainBackground { get; set; }
+        public string CardBackground { get; set; }
+    }
+}
